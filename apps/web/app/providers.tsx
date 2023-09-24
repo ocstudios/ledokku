7729ab778b02cd6d02667d16ee104e2121f3d409
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2023-09-24 00:21:42 -0600

    Update providers.tsx

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,36 +1,32 @@
-'use client'
+"use client";
 
-import apollo from '@/lib/apollo'
-import { ApolloProvider } from '@apollo/client'
-import { NextUIProvider } from '@nextui-org/react'
-import { Session } from 'next-auth'
-import { SessionProvider } from 'next-auth/react'
-import NextProgress from 'next-progress'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { Toaster } from 'react-hot-toast'
-
+import apollo from "@/lib/apollo";
+import { ApolloProvider } from "@apollo/client";
+import { NextUIProvider } from "@nextui-org/react";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import NextProgress from "next-progress";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
 
 export function Providers({
   children,
-  session
+  session,
 }: {
-  children: React.ReactNode
-  session: Session | null
+  children: React.ReactNode;
+  session: Session | null;
 }) {
   return (
     <SessionProvider session={session}>
-            <ApolloProvider client={apollo}>
-                <NextThemesProvider
-                    defaultTheme="system"
-                    attribute="class"
-                >
-                    <NextUIProvider>
-                        {children}
-                        <NextProgress />
-                    </NextUIProvider>
-                </NextThemesProvider>
-                <Toaster position='top-right' />
-            </ApolloProvider>
-        </SessionProvider>
-  )
+      <ApolloProvider client={apollo}>
+        <NextThemesProvider defaultTheme="system" attribute="class">
+          <NextUIProvider>
+            {children}
+            <NextProgress />
+          </NextUIProvider>
+        </NextThemesProvider>
+        <Toaster position="top-right" />
+      </ApolloProvider>
+    </SessionProvider>
+  );
 }
