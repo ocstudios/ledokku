0da37fbecfbdf6d426ed046269635b6695ee5039
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2023-09-23 22:18:54 -0600

    Update webhook.controller.ts

--- apps/server/src/controllers/webhook.controller.ts
+++ apps/server/src/controllers/webhook.controller.ts
@@ -1,39 +1,43 @@
-import { $log, BodyParams, HeaderParams, RawBodyParams } from '@tsed/common';
-import { Controller } from '@tsed/di';
-import { InternalServerError, Unauthorized } from '@tsed/exceptions';
-import { ContentType, Post, Returns } from '@tsed/schema';
-import { verifyWebhookSecret } from '../lib/webhooks/verifyGithubSecret';
-import { GithubRepository } from './../modules/github/data/repositories/github.repository';
+import { $log, BodyParams, HeaderParams, RawBodyParams } from "@tsed/common";
+import { Controller } from "@tsed/di";
+import { InternalServerError, Unauthorized } from "@tsed/exceptions";
+import { ContentType, Post, Returns } from "@tsed/schema";
+import { verifyWebhookSecret } from "../lib/webhooks/verifyGithubSecret";
+import { GithubRepository } from "./../modules/github/data/repositories/github.repository";
 
 class GitNode {
   id: number;
+  node_id: string;
 }
 
-@Controller('/webhooks')
+@Controller("/webhooks")
 export class WebhookController {
   constructor(private githubRepository: GithubRepository) {}
 
   @Post()
-  @ContentType('application/json')
+  @ContentType("application/json")
   @Returns(200)
   async onMessage(
-    @HeaderParams('x-github-event') githubEvent: string,
-    @HeaderParams('x-hub-signature-256') secret: string,
-    @BodyParams('installation', GitNode) installation: GitNode,
-    @BodyParams('repository', GitNode) repository: GitNode,
-    @BodyParams('ref') ref: string,
-    @BodyParams('pusher') pusher: any,
+    @HeaderParams("x-github-event") githubEvent: string,
+    @HeaderParams("x-hub-signature-256") secret: string,
+    @BodyParams("installation", GitNode) installation: GitNode,
+    @BodyParams("repository", GitNode) repository: GitNode,
+    @BodyParams("ref") ref: string,
+    @BodyParams("pusher") pusher: any,
     @RawBodyParams()
     rawBody: any
   ): Promise<any> {
-    if (githubEvent === 'push') {
+    console.log("installation", installation);
+    console.log("repository", repository);
+
+    if (githubEvent === "push") {
       try {
         await this.handleWebhooks(
           secret,
           rawBody,
           installation.id.toString(),
           repository.id.toString(),
-          ref.replace('refs/heads/', ''),
+          ref.replace("refs/heads/", ""),
           pusher.name
         );
       } catch (e) {
@@ -55,7 +59,7 @@ export class WebhookController {
     const requestVerified = verifyWebhookSecret(secret, rawBody);
 
     if (!requestVerified) {
-      throw new Unauthorized('Invalid request');
+      throw new Unauthorized("Invalid request");
     }
 
     return this.githubRepository.deployRepository(
